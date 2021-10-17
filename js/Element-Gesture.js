f1abde01ea069840b3f9bec7743230622c57707e
bbggkkk <bbggkkk@naver.com>
2021-10-17 23:50:16 +0900

    수정

--- js/Element-Gesture.js
+++ js/Element-Gesture.js
@@ -1,203 +1,196 @@
-"use strict";
-/**
- *
- *
-필요값
+(function(){
 
 
-drag
-
-시작지점
-startX,startY
-
-시작으로부터 현재까지의 이동거리
-distanceX,distanceY,distance
-
-이전 틱과의 이동거리
-moveX,moveY,move
-
-현재 위치
-positionX,positionY
-
-이전 위치
-prePositionX,prePositionY
-
-속도 (기준 잡아야함 (ex. 초당 이동한 픽셀))
-speedX,speedY,speed
-
-이동 방향
-direction
-
-총 이동거리
-distanceAll
-
-*/
-HTMLElement.prototype.gesture = function ($event) {
-    var _this = this;
-    var eventMap = {
-        mousedown: 'dragStart',
-        touchstart: 'dragStart',
-        mousemove: 'drag',
-        touchmove: 'drag',
-        mouseup: 'dragEnd',
-        touchend: 'dragEnd',
+    const eventMap = {
+        dragStart   : ['touchstart', 'mousedown'],
+        drag        : ['touchmove', 'mousemove'],
+        dragEnd     : ['touchend', 'mouseup']
     };
-    var type = Object.keys($event);
-    this.gestureData = {};
-    var md = function (event) {
-        var mousemove = function (event) {
-            requestAnimationFrame(function () {
-                var clientX = event.clientX, clientY = event.clientY, moveX = event.movementX, moveY = event.movementY;
-                var distance1 = clientX - _this.gestureData.dragStart.start[0];
-                var distance2 = clientY - _this.gestureData.dragStart.start[1];
-                var distance3 = Math.sqrt(Math.abs(distance1 * distance1) + Math.abs(distance2 * distance2));
-                var move3 = Math.sqrt(Math.abs(moveX * moveX) + Math.abs(moveY * moveY));
-                _this.gestureData.drag = {
-                    start: _this.gestureData.dragStart.start,
-                    // offset      : this.gestureData.dragStart.offset,
-                    distance: [
-                        distance1,
-                        distance2,
-                        distance3
-                    ],
-                    move: [
-                        moveX,
-                        moveY,
-                        move3
-                    ],
-                    prePosition: [
-                        _this.gestureData.drag ? _this.gestureData.drag.position[0] : clientX,
-                        _this.gestureData.drag ? _this.gestureData.drag.position[1] : clientY
-                    ],
-                    position: [
-                        clientX,
-                        clientY
-                    ],
-                    direction: [
-                        moveX > 0 ? 1 : moveX < 0 ? -1 : 0,
-                        moveY > 0 ? 1 : moveY < 0 ? -1 : 0
-                    ],
-                    distanceAll: _this.gestureData.drag ? _this.gestureData.drag.distanceAll + move3 : 0,
-                    type: 'drag'
-                };
-                if ($event[eventMap[event.type]] && eventMap[event.type] === 'drag') {
-                    $event[eventMap[event.type]](_this.gestureData.drag, _this, event);
-                }
+
+    let gestureData = {
+        start       : [0, 0],
+        distance    : [0, 0, 0],
+        move        : [0, 0, 0],
+        prePosition : [0, 0],
+        position    : [0, 0],
+        direction   : [0, 0],
+        distanceAll : 0,
+        type        : ''
+    };
+    let globalEvent = {};
+    
+    const util = {
+        bind : function($this, event){
+            const fn = this.getEvent($this, event);
+            $this.addEventListener('touchstart', fn.touchstart, {passive:false});
+            $this.addEventListener('mousedown', fn.mousedown, {passive:false});
+        },
+        unbind : function($this, event){
+            const eventList   = event === undefined ? ['dragStart', 'drag', 'dragEnd'] : event;
+            const removeEvent = eventList.reduce((acc,item) => {
+                acc[item] = () => {};
+                return acc;
+            },[]);
+            this.removeEvent($this, eventList);
+            // this.bind($this, removeEvent);
+        },
+        removeEvent : function($this, eventNameList){
+            eventNameList.forEach(item => {
+                eventMap[item].forEach(eventName => {
+                    $this.removeEventListener(eventName, globalEvent[eventName]);
+                });
             });
-        };
-        var mouseup = function (event) {
-            document.removeEventListener('mousemove', mousemove);
-            document.removeEventListener('mouseup', mouseup);
-            if ($event[eventMap[event.type]] && eventMap[event.type] === 'dragEnd') {
-                _this.gestureData.dragEnd = _this.gestureData.drag ? _this.gestureData.drag : _this.gestureData.dragStart;
-                _this.gestureData.dragEnd.type = 'dragEnd';
-                $event[eventMap[event.type]](_this.gestureData.dragEnd, _this, event);
+        },
+        getEvent : function($this, event){
+            const fn = {
+                touchstart : (e) => {
+                    const callback = event.dragStart;
+                    eventFn.touchstart(e, callback, $this);
+                },
+                touchmove : (e) => {
+                    const callback = event.drag;
+                    eventFn.touchmove(e, callback, $this);
+                },
+                touchend : (e) => {
+                    const callback = event.dragEnd;
+                    eventFn.touchend(e, callback, $this);
+                },
+                mousedown : (e) => {
+                    const callback = event.dragStart;
+                    eventFn.mousedown(e, callback, $this);
+                },
+                mousemove : (e) => {
+                    const callback = event.drag;
+                    eventFn.mousemove(e, callback, $this);
+                },
+                mouseup : (e) => {
+                    const callback = event.dragEnd;
+                    eventFn.mouseup(e, callback, $this);
+                },
             }
-            _this.gestureData.drag && (_this.gestureData.drag = undefined);
-        };
-        _this.gestureData.dragStart = {
-            start: [event.clientX, event.clientY],
-            // offset      : [event.offsetX,event.offsetY],
-            distance: [0, 0, 0],
-            move: [0, 0, 0],
-            position: [event.clientX, event.clientY],
-            prePosition: [event.clientX, event.clientY],
-            direction: [0, 0],
-            distanceAll: 0,
-            type: 'dragStart'
-        };
-        if ($event[eventMap[event.type]] && eventMap[event.type] === 'dragStart') {
-            $event[eventMap[event.type]](_this.gestureData.dragStart, _this, event);
+            globalEvent = fn;
+            return fn;
         }
-        document.addEventListener('mousemove', mousemove);
-        document.addEventListener('mouseup', mouseup);
-    };
-    var td = function (event) {
-        var touchmove = function (event) {
-            requestAnimationFrame(function () {
-                if (event.cancelable) {
-                    event.preventDefault();
-                }
-                var clientX = event.touches[0].clientX;
-                var clientY = event.touches[0].clientY;
-                var moveX = _this.gestureData.drag ? clientX - _this.gestureData.drag.position[0] : 0;
-                var moveY = _this.gestureData.drag ? clientY - _this.gestureData.drag.position[1] : 0;
-                var distance1 = clientX - _this.gestureData.dragStart.start[0];
-                var distance2 = clientY - _this.gestureData.dragStart.start[1];
-                var distance3 = Math.sqrt(Math.abs(distance1 * distance1) + Math.abs(distance2 * distance2));
-                var move3 = Math.sqrt(Math.abs(moveX * moveX) + Math.abs(moveY * moveY));
-                _this.gestureData.drag = {
-                    start: _this.gestureData.dragStart.start,
-                    // offset      : this.gestureData.dragStart.offset,
-                    distance: [
-                        distance1,
-                        distance2,
-                        distance3
-                    ],
-                    move: [
-                        moveX,
-                        moveY,
-                        move3
-                    ],
-                    prePosition: [
-                        _this.gestureData.drag ? _this.gestureData.drag.position[0] : clientX,
-                        _this.gestureData.drag ? _this.gestureData.drag.position[1] : clientY
-                    ],
-                    position: [
-                        clientX,
-                        clientY
-                    ],
-                    direction: [
-                        moveX > 0 ? 1 : moveX < 0 ? -1 : 0,
-                        moveY > 0 ? 1 : moveY < 0 ? -1 : 0
-                    ],
-                    distanceAll: _this.gestureData.drag ? _this.gestureData.drag.distanceAll + move3 : 0,
-                    type: 'drag'
-                };
-                if ($event[eventMap[event.type]] && eventMap[event.type] === 'drag') {
-                    $event[eventMap[event.type]](_this.gestureData.drag, _this, event);
-                }
-            });
-        };
-        var touchend = function (event) {
-            // if (event.cancelable) {
-            //     event.preventDefault();
-            // }
-            document.removeEventListener('touchmove', touchmove);
-            document.removeEventListener('touchend', touchend);
-            if ($event[eventMap[event.type]] && eventMap[event.type] === 'dragEnd') {
-                // console.log(_this.gestureData,_this.gestureData.dragEnd);
-                _this.gestureData.dragEnd = _this.gestureData.drag ? _this.gestureData.drag : _this.gestureData.dragStart;
-                _this.gestureData.dragEnd.type = 'dragEnd';
-                $event[eventMap[event.type]](_this.gestureData.dragEnd, _this, event);
-            }
-            _this.gestureData.drag && (_this.gestureData.drag = undefined);
-        };
-        var clientX = event.touches[0].clientX;
-        var clientY = event.touches[0].clientY;
-        _this.gestureData.dragStart = {
-            start: [clientX, clientY],
-            // offset      : [offsetX,offsetY],
-            distance: [0, 0, 0],
-            move: [0, 0, 0],
-            position: [clientX, clientY],
-            prePosition: [clientX, clientY],
-            direction: [0, 0],
-            distanceAll: 0,
-            type: 'dragStart'
-        };
-        if ($event[eventMap[event.type]] && eventMap[event.type] === 'dragStart') {
-            $event[eventMap[event.type]](_this.gestureData.dragStart, _this, event);
-        }
-        document.addEventListener('touchmove', touchmove);
-        document.addEventListener('touchend', touchend);
-    };
-    if ($event === false) {
-        this.removeEventListener('mousedown', md);
-        this.removeEventListener('touchstart', td);
-        return;
     }
-    this.addEventListener('mousedown', md, { passive: true });
-    this.addEventListener('touchstart', td, { passive: true });
-};
-//# sourceMappingURL=elementGesture.js.map
+    const eventFn = {
+        touchstart : (e, callback, $this) => {
+            e.preventDefault();
+            const data = {
+                start       : [e.touches[0].clientX, e.touches[0].clientY],
+                distance    : [0, 0, 0],
+                move        : [0, 0, 0],
+                position    : [e.touches[0].clientX, e.touches[0].clientY],
+                prePosition : [e.touches[0].clientX, e.touches[0].clientY],
+                direction   : [0, 0],
+                distanceAll : 0,
+                type        : 'dragStart'
+            }
+            gestureData = data;
+            callback(data, $this, e);
+            document.addEventListener('touchmove', globalEvent['touchmove'], {passive:false});
+            document.addEventListener('touchend', globalEvent['touchend']), {passive:false};
+        },
+        touchmove : (e, callback, $this) => {
+            e.preventDefault();
+            requestAnimationFrame(() => {
+                const clientX   = e.touches[0].clientX,
+                      clientY   = e.touches[0].clientY,
+                      moveX     = gestureData ? clientX - gestureData.position[0] : 0,
+                      moveY     = gestureData ? clientY - gestureData.position[1] : 0;
+                const distance1 = clientX - gestureData.start[0];
+                const distance2 = clientY - gestureData.start[1];
+                const distance3 = Math.sqrt(Math.abs(distance1 * distance1) + Math.abs(distance2 * distance2));
+                const move3     = Math.sqrt(Math.abs(moveX * moveX) + Math.abs(moveY * moveY));
+
+                const data = {
+                    start       : [gestureData.start[0], gestureData.start[1]],
+                    distance    : [distance1, distance2, distance3],
+                    move        : [moveX, moveY, move3],
+                    position    : [clientX, clientY],
+                    prePosition : [gestureData ? gestureData.position[0] : clientX, gestureData ? gestureData.position[1] : clientY],
+                    direction   : [moveX > 0 ? 1 : moveX < 0 ? -1 : 0, moveY > 0 ? 1 : moveY < 0 ? -1 : 0],
+                    distanceAll : gestureData ? gestureData.distanceAll + move3 : 0,
+                    type        : 'drag'
+                }
+                gestureData = data;
+                callback(data, $this, e);
+
+            });
+        },
+        touchend : (e, callback, $this) => {
+            e.preventDefault();
+            document.removeEventListener('touchmove', globalEvent['touchmove']);
+            document.removeEventListener('touchend', globalEvent['touchend']);
+            const data = Object.assign({}, gestureData, {type:'dragEnd'})
+            gestureData = data;
+            callback(data, $this, e);
+        },
+        mousedown : (e, callback, $this) => {
+            const data = {
+                start       : [e.clientX, e.clientY],
+                distance    : [0, 0, 0],
+                move        : [0, 0, 0],
+                position    : [e.clientX, e.clientY],
+                prePosition : [e.clientX, e.clientY],
+                direction   : [0, 0],
+                distanceAll : 0,
+                type        : 'dragStart'
+            }
+            gestureData = data;
+            callback(data, $this, e);
+            document.addEventListener('mousemove', globalEvent['mousemove'], {passive:false});
+            document.addEventListener('mouseup', globalEvent['mouseup']), {passive:false};
+        },
+        mousemove : (e, callback, $this) => {
+            e.preventDefault();
+            requestAnimationFrame(() => {
+                const clientX   = e.clientX,
+                      clientY   = e.clientY,
+                      moveX     = e.movementX,
+                      moveY     = e.movementY;
+                const distance1 = clientX - gestureData.start[0];
+                const distance2 = clientY - gestureData.start[1];
+                const distance3 = Math.sqrt(Math.abs(distance1 * distance1) + Math.abs(distance2 * distance2));
+                const move3     = Math.sqrt(Math.abs(moveX * moveX) + Math.abs(moveY * moveY));
+
+                const data = {
+                    start       : [gestureData.start[0], gestureData.start[1]],
+                    distance    : [distance1, distance2, distance3],
+                    move        : [moveX, moveY, move3],
+                    position    : [clientX, clientY],
+                    prePosition : [gestureData ? gestureData.position[0] : clientX, gestureData ? gestureData.position[1] : clientY],
+                    direction   : [moveX > 0 ? 1 : moveX < 0 ? -1 : 0, moveY > 0 ? 1 : moveY < 0 ? -1 : 0],
+                    distanceAll : gestureData ? gestureData.distanceAll + move3 : 0,
+                    type        : 'drag'
+                }
+                gestureData = data;
+                callback(data, $this, e);
+
+            });
+        },
+        mouseup : (e, callback, $this) => {
+            e.preventDefault();
+            document.removeEventListener('mousemove', globalEvent['mousemove']);
+            document.removeEventListener('mouseup', globalEvent['mouseup']);
+            const data = Object.assign({}, gestureData, {type:'dragEnd'})
+            gestureData = data;
+            callback(data, $this, e);
+        },
+    }
+
+    HTMLElement.prototype.gesture = function({
+        dragStart   = () => {},
+        drag        = () => {},
+        dragEnd     = () => {}
+    }){
+        util.bind(this, {
+            dragStart,
+            drag,
+            dragEnd
+        });
+        
+        return util;
+    }
+
+
+})();
